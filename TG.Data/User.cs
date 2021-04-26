1c3ba2c396f4153616dafe15f8f54362ced5aad5
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-04-26 10:11:25 +0300

    Add files via upload

---
+++ TG.Data/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TG.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public long TGId { get; set; }
+        public string Status { get; set; }
+    }
+}
