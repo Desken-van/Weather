87737002d8031b08ac010a9c90ff7319f42cd7b9
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-04-26 09:58:17 +0300

    Add files via upload

---
+++ TG.Entity/UserContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace TG.Entity
+{
+    public class UserContext :DbContext
+    {
+        public DbSet<UserEntity> Users { get; set; }
+        public UserContext(DbContextOptions<UserContext> options)
+                : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+            modelBuilder.Entity<UserEntity>(entity =>
+            {
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+                
+                entity.Property(e => e.Status)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+            });
+
+        }
+    }
+}
+
