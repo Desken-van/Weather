5f296be009b3f0bb803631eebeb88d3b65eac66a
Desken-van <79146520+Desken-van@users.noreply.github.com>
2021-04-26 10:12:37 +0300

    Add files via upload

---
+++ TG.Interface/IUserRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TG.Data;
+using TG.Entity;
+
+namespace TG.Interface
+{
+    public interface IUserRepository : IDisposable
+    {
+        Task<IEnumerable<User>> GetUserList();
+        Task<User> GetUser(int id);
+        Task<User> GetTG(long id);
+        Task Add(string firstname, string lastname, string username, long tgid);
+        Task Create(UserEntity item);
+        Task Update(User item);
+        Task Delete(int id);
+    }
+}
